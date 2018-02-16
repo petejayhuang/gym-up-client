7cbc5e5ebf09da0f54e39ce959a30facf46829fa
Pete Huang <petejayhuang@users.noreply.github.com>
2018-02-16 16:53:27 +0000

    get server ready

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require("express");
+const path = require("path");
 
 var app = express();
 
-app.use(express.static(__dirname + "/client/dist"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/client/dist"));
+}
 
 app.get("*", (req, res) => {
-  res.sendfile("./client/dist/index.html");
+  console.log("* route hit");
+  res.sendFile(path.join(__dirname, "./client", "/dist", "/index.html"));
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started!");
+});
